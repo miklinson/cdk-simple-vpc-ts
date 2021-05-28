b6e23b19e655bde81b8de730f2855c7c9f5d90ec
Mik Linson <miklinson@gmail.com>
2021-05-28 14:00:42 +0800

    Added VPC details

--- lib/vpc-dev-stack.ts
+++ lib/vpc-dev-stack.ts
@@ -1,9 +1,38 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2'
 
 export class VpcDevStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'VPCDev', {
+      cidr: "10.100.0.0/16",
+      maxAzs: 2,
+      subnetConfiguration: [
+        {
+          // 'subnetType' controls Internet access, as described above.
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: 'Ingress',
+          cidrMask: 24,
+        },
+        {
+          subnetType: ec2.SubnetType.PRIVATE,
+          name: 'Application',
+          cidrMask: 24,
+        },
+        {
+          cidrMask: 26,
+          name: 'Database',
+          subnetType: ec2.SubnetType.ISOLATED,
+    
+          // 'reserved' can be used to reserve IP address space. No resources will
+          // be created for this subnet, but the IP range will be kept available for
+          // future creation of this subnet, or even for future subdivision.
+          reserved: true
+        }
+      ],
+   })
+   
   }
 }
